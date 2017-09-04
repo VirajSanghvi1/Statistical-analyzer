2386eb78afe506e470a6af280057bedd77cf93fe
VirajSanghvi12 <v.h.sanghvi12@gmail.com>
2017-09-03 22:42:39 -0500

    Data displaying on an editable table
    Data displaying has been implemented and tested.. but edits to the
    table are not recorded

--- GUI test.py
+++ GUI test.py
@@ -1,7 +1,6 @@
 import sys
 import pandas as pd
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QInputDialog, QLineEdit, QFileDialog
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QTableWidget, QTableWidgetItem, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
 
 
@@ -12,8 +11,8 @@ class App(QWidget):
         self.title = 'PyQt5 simple window - pythonspot.com'
         self.left = 10
         self.top = 10
-        self.width = 640
-        self.height = 480
+        self.width = 660
+        self.height = 500
         self.initUI()
 
     def initUI(self):
@@ -22,11 +21,12 @@ class App(QWidget):
 
         button = QPushButton('Open a file', self)
         button.setToolTip('Load a data file')
-        button.move(535, 450)
+        button.move(560, 475)
         button.clicked.connect(self.on_click)
 
         self.show()
 
+    #Function used to open the File Dialog so that the user can select a file
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options = QFileDialog.DontUseNativeDialog
@@ -36,10 +36,41 @@ class App(QWidget):
             print(fileName)
             return(fileName)
 
+    #Creates the editable table using PyQt and Pandas Dataframe
+    def createTable(self, df):
+        #row and column count
+        rows, columns = df.shape
+
+        #horizontal and vertical labels
+        row_label = list(df.index)
+        columns_label = list(df.columns.values)
+
+        #create the table
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(rows)
+        self.tableWidget.setColumnCount(columns)
+
+        #add information to the table
+        for i in range(rows):
+            self.tableWidget.setVerticalHeaderItem(i, QTableWidgetItem(row_label[i]))
+            for j in range(columns):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(df.get_value(row_label[i],columns_label[j]))))
+
+                if i < 1:
+                    self.tableWidget.setHorizontalHeaderItem(j, QTableWidgetItem(columns_label[j]))
+
+        #new Boxlayout for displaying the table and the table to it
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
+
     @pyqtSlot()
     def on_click(self):
         filename = self.openFileNameDialog()
         df = pd.read_csv(filename)
+
+        self.createTable(df)
+
         print(df)
         print('Clicked')
 
